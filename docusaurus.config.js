650c727e07cef7abe3b39f771532e65f2bf36c25
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2022-10-24 17:13:20 -0700

    deploy command + W&B are cool

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'W&B are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
