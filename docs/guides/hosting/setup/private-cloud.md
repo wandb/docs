4cf4f6593dad17007afe45c914b6b140ac9bab85
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2022-11-17 17:34:02 -0800

    Added hosting chapter

---
+++ docs/guides/hosting/setup/private-cloud.md
@@ -0,0 +1,35 @@
+---
+description: Hosting W&B Server on your private cloud.
+---
+
+# Private Cloud
+
+A W&BServer running in a scalable deployment on your private AWS or GCP infrastructure, in your chosen region, and connected to a scalable data store. The environment can be provisioned by us or by your company, using a toolset comprised of Terraform and Kubernetes.
+
+Talk to our sales team by reaching out to [contact@wandb.com](mailto:contact@wandb.com).
+
+## Requirements
+
+A private cloud deployment requires the following cloud resources in your account:
+
+* A Kubernetes cluster (EKS or GKE)
+* A SQL database (RDS, Google Cloud SQL, or Azure MySQL)
+* A object store (S3, GCS, or Azure Blob Store)
+
+:::info
+Check out a [video tutorial](https://www.youtube.com/watch?v=bYmLY5fT2oA) for getting set up using [Terraform](https://www.terraform.io) on AWS!
+:::
+
+<!-- {% embed url="https://www.youtube.com/watch?v=bYmLY5fT2oA" %} -->
+
+## Amazon Web Services
+
+The simplest way to configure W&Bwithin AWS is to use our [official Terraform](https://github.com/wandb/terraform-aws-wandb). Detailed instructions can be found in the README. If instead you want to configure services manually you can find [instructions here](configuration.md#amazon-web-services).
+
+## Microsoft Azure
+
+The simplest way to configure W&Bwithin Azure is to use our [official Terraform](https://github.com/wandb/terraform-azurerm-wandb). Detailed instructions can be found in the README. If instead you want to configure services manually you can find [instructions here](configuration.md#azure).
+
+## Google Cloud Platform
+
+The simplest way to configure W&Bwithin GCP is to use our [official Terraform](https://github.com/wandb/terraform-google-wandb). Detailed instructions can be found in the README. If instead you want to configure services manually you can find [instructions here](configuration.md#google-cloud-platform).
--- sidebars.js
+++ sidebars.js
@@ -53,198 +53,218 @@ const sidebars = {
         }
       ]
     },
-      {
-        type: 'category',
-        label: 'Artifacts',
-        items: [
-          'guides/artifacts/intro',
-          'guides/artifacts/quickstart',
-          'guides/artifacts/construct-an-artifact',
-          'guides/artifacts/download-and-use-an-artifact',
-          'guides/artifacts/update-an-artifact',
-          'guides/artifacts/create-a-custom-alias',
-          'guides/artifacts/create-a-new-artifact-version',
-          'guides/artifacts/track-external-files',
-          'guides/artifacts/delete-artifacts',
-          'guides/artifacts/explore-and-traverse-an-artifact-graph',
-          'guides/artifacts/storage',
-          'guides/artifacts/data-privacy-and-compliance',
-          // 'guides/artifacts/artifacts-faqs',
-          'guides/artifacts/examples',
-        ]
-      },
-      {
-        type: 'category',
-        label: 'Tune Hyperparameters',
-        items: [
-          'guides/tune-hyperparameters/intro',
-          'guides/tune-hyperparameters/quickstart',
-          'guides/tune-hyperparameters/add-w-and-b-to-your-code',
-          'guides/tune-hyperparameters/define-sweep-configuration',
-          'guides/tune-hyperparameters/initialize-sweeps',
-          'guides/tune-hyperparameters/start-sweep-agents',
-          'guides/tune-hyperparameters/parallelize-agents',
-          'guides/tune-hyperparameters/visualize-sweep-results',
-          'guides/tune-hyperparameters/pause-resume-and-cancel-sweeps',
-          'guides/tune-hyperparameters/sweeps-ui',
-          'guides/tune-hyperparameters/local-controller',
-          'guides/tune-hyperparameters/troubleshoot-sweeps',
-          'guides/tune-hyperparameters/faq',
-          'guides/tune-hyperparameters/useful-resources',
-          'guides/tune-hyperparameters/existing-project',
-        ]
-      },
-      {
-        type: 'category',
-        label: 'Collaborative Reports',
-        items: [
-          'guides/reports/intro',
-          'guides/reports/create-a-report',
-          'guides/reports/edit-a-report',
-          'guides/reports/collaborate-on-reports',
-          'guides/reports/clone-and-export-reports',
-          'guides/reports/embed-reports',
-          'guides/reports/cross-project-reports',
-          'guides/reports/reports-faq',
-        ]
-      },
-      {
-        type: 'category',
-        label: 'Data and model versioning',
-        items: [
-          'guides/data-and-model-versioning/intro',
-          'guides/data-and-model-versioning/dataset-versioning',
-          'guides/data-and-model-versioning/model-versioning',
-        ]
-      },
-      {
-        type: 'category',
-        label: 'Model Management',
-        items: [
-          'guides/models/intro',
-          'guides/models/model-management-concepts',
-          'guides/models/walkthrough',
-        ]
-      },
-      {
-        type: 'category',
-        label: 'Data Visualization',
-        items: [
-          'guides/data-vis/intro',
-          'guides/data-vis/tables-quickstart',
-          'guides/data-vis/log-tables',
-          'guides/data-vis/tables',
-        ]
-      },
-      {
-        type: 'category',
-        label: 'W&B App UI',
-        items: [
-          'guides/app/README',
-          {
-            type: 'category',
-            label: 'Features',
-            items: [
-              // 'guides/app/features/intro',
-              {
-                type: 'category',
-                label: 'Panels',
-                items: [
-                  'guides/app/features/panels/intro',
-                  {
-                    type: 'category',
-                    label: 'Line Plot',
-                    items: [
-                      'guides/app/features/panels/line-plot/intro',
-                      'guides/app/features/panels/line-plot/reference',
-                      'guides/app/features/panels/line-plot/sampling',
-                      'guides/app/features/panels/line-plot/smoothing',
-                    ]
-                  },
-                  'guides/app/features/panels/bar-plot',
-                  'guides/app/features/panels/run-colors',
-                  'guides/app/features/panels/parallel-coordinates',
-                  'guides/app/features/panels/scatter-plot',
-                  'guides/app/features/panels/code',
-                  'guides/app/features/panels/parameter-importance',
-                  'guides/app/features/panels/run-comparer',
-                  {
-                    type: 'category',
-                    label: 'Weave',
-                    items: [
-                      'guides/app/features/panels/weave/intro',
-                      'guides/app/features/panels/weave/embedding-projector',
-                    ]
-                  },
-                ]
-              },
-              {
-                type: 'category',
-                label: 'Custom Charts',
-                items: [
-                  'guides/app/features/custom-charts/intro',
-                  'guides/app/features/custom-charts/walkthrough',
-                ]
-              },
-              'guides/app/features/runs-table',
-              'guides/app/features/tags',
-              'guides/app/features/notes',
-              // 'guides/app/features/alerts',
-              'guides/app/features/teams',
-              'guides/app/features/system-metrics',
-              'guides/app/features/anon',
-            ]
-          },
-          {
-            type: 'category',
-            label: 'Pages',
-            items: [
-              'guides/app/pages/intro',
-              'guides/app/pages/gradient-panel',
-              'guides/app/pages/project-page',
-              'guides/app/pages/run-page',
-              'guides/app/pages/workspaces',
-            ]
-          },
-          {
-            type: 'category',
-            label: 'Settings',
-            items: [
-              'guides/app/settings-page/intro',
-              'guides/app/settings-page/user-settings',
-              'guides/app/settings-page/team-settings',
-              'guides/app/settings-page/emails',
-            ]
-          },
-        ]
-      },
-      {
-        type: 'category',
-        label: 'Integrations',
-        items: [
-          'guides/integrations/intro',
-          'guides/integrations/keras',
-          'guides/integrations/pytorch',
-          'guides/integrations/lightning',
-          'guides/integrations/tensorflow',
-          'guides/integrations/tensorboard',
-          'guides/integrations/huggingface',
-          'guides/integrations/spacy',
-          'guides/integrations/yolov5',
-          'guides/integrations/mmdetection',
-          'guides/integrations/scikit',
-          'guides/integrations/xgboost',
-          'guides/integrations/lightgbm',
-          {
-            type: 'category',
-            label: 'Fastai',
-            items: [
-              'guides/integrations/fastai/intro',
-              'guides/integrations/fastai/v1',
-            ]
-          },
-        ]
-      },
+    {
+      type: 'category',
+      label: 'Artifacts',
+      items: [
+        'guides/artifacts/intro',
+        'guides/artifacts/quickstart',
+        'guides/artifacts/construct-an-artifact',
+        'guides/artifacts/download-and-use-an-artifact',
+        'guides/artifacts/update-an-artifact',
+        'guides/artifacts/create-a-custom-alias',
+        'guides/artifacts/create-a-new-artifact-version',
+        'guides/artifacts/track-external-files',
+        'guides/artifacts/delete-artifacts',
+        'guides/artifacts/explore-and-traverse-an-artifact-graph',
+        'guides/artifacts/storage',
+        'guides/artifacts/data-privacy-and-compliance',
+        // 'guides/artifacts/artifacts-faqs',
+        'guides/artifacts/examples',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Tune Hyperparameters',
+      items: [
+        'guides/tune-hyperparameters/intro',
+        'guides/tune-hyperparameters/quickstart',
+        'guides/tune-hyperparameters/add-w-and-b-to-your-code',
+        'guides/tune-hyperparameters/define-sweep-configuration',
+        'guides/tune-hyperparameters/initialize-sweeps',
+        'guides/tune-hyperparameters/start-sweep-agents',
+        'guides/tune-hyperparameters/parallelize-agents',
+        'guides/tune-hyperparameters/visualize-sweep-results',
+        'guides/tune-hyperparameters/pause-resume-and-cancel-sweeps',
+        'guides/tune-hyperparameters/sweeps-ui',
+        'guides/tune-hyperparameters/local-controller',
+        'guides/tune-hyperparameters/troubleshoot-sweeps',
+        'guides/tune-hyperparameters/faq',
+        'guides/tune-hyperparameters/useful-resources',
+        'guides/tune-hyperparameters/existing-project',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Collaborative Reports',
+      items: [
+        'guides/reports/intro',
+        'guides/reports/create-a-report',
+        'guides/reports/edit-a-report',
+        'guides/reports/collaborate-on-reports',
+        'guides/reports/clone-and-export-reports',
+        'guides/reports/embed-reports',
+        'guides/reports/cross-project-reports',
+        'guides/reports/reports-faq',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Data and model versioning',
+      items: [
+        'guides/data-and-model-versioning/intro',
+        'guides/data-and-model-versioning/dataset-versioning',
+        'guides/data-and-model-versioning/model-versioning',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Model Management',
+      items: [
+        'guides/models/intro',
+        'guides/models/model-management-concepts',
+        'guides/models/walkthrough',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Data Visualization',
+      items: [
+        'guides/data-vis/intro',
+        'guides/data-vis/tables-quickstart',
+        'guides/data-vis/log-tables',
+        'guides/data-vis/tables',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'W&B App UI',
+      items: [
+        'guides/app/README',
+        {
+          type: 'category',
+          label: 'Features',
+          items: [
+            // 'guides/app/features/intro',
+            {
+              type: 'category',
+              label: 'Panels',
+              items: [
+                'guides/app/features/panels/intro',
+                {
+                  type: 'category',
+                  label: 'Line Plot',
+                  items: [
+                    'guides/app/features/panels/line-plot/intro',
+                    'guides/app/features/panels/line-plot/reference',
+                    'guides/app/features/panels/line-plot/sampling',
+                    'guides/app/features/panels/line-plot/smoothing',
+                  ]
+                },
+                'guides/app/features/panels/bar-plot',
+                'guides/app/features/panels/run-colors',
+                'guides/app/features/panels/parallel-coordinates',
+                'guides/app/features/panels/scatter-plot',
+                'guides/app/features/panels/code',
+                'guides/app/features/panels/parameter-importance',
+                'guides/app/features/panels/run-comparer',
+                {
+                  type: 'category',
+                  label: 'Weave',
+                  items: [
+                    'guides/app/features/panels/weave/intro',
+                    'guides/app/features/panels/weave/embedding-projector',
+                  ]
+                },
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Custom Charts',
+              items: [
+                'guides/app/features/custom-charts/intro',
+                'guides/app/features/custom-charts/walkthrough',
+              ]
+            },
+            'guides/app/features/runs-table',
+            'guides/app/features/tags',
+            'guides/app/features/notes',
+            // 'guides/app/features/alerts',
+            'guides/app/features/teams',
+            'guides/app/features/system-metrics',
+            'guides/app/features/anon',
+          ]
+        },
+        {
+          type: 'category',
+          label: 'Pages',
+          items: [
+            'guides/app/pages/intro',
+            'guides/app/pages/gradient-panel',
+            'guides/app/pages/project-page',
+            'guides/app/pages/run-page',
+            'guides/app/pages/workspaces',
+          ]
+        },
+        {
+          type: 'category',
+          label: 'Settings',
+          items: [
+            'guides/app/settings-page/intro',
+            'guides/app/settings-page/user-settings',
+            'guides/app/settings-page/team-settings',
+            'guides/app/settings-page/emails',
+          ]
+        },
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Private Hosting',
+      items: [
+        'guides/hosting/intro',
+        'guides/hosting/basic-setup',
+        {
+          type: 'category',
+          label: 'Production Setup',
+          items: [
+            'guides/hosting/setup/intro',
+            'guides/hosting/setup/dedicated-cloud',
+            'guides/hosting/setup/private-cloud',
+            'guides/hosting/setup/on-premise-baremetal',
+            'guides/hosting/setup/configuration',
+          ]
+        },
+        'guides/hosting/faq',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Integrations',
+      items: [
+        'guides/integrations/intro',
+        'guides/integrations/keras',
+        'guides/integrations/pytorch',
+        'guides/integrations/lightning',
+        'guides/integrations/tensorflow',
+        'guides/integrations/tensorboard',
+        'guides/integrations/huggingface',
+        'guides/integrations/spacy',
+        'guides/integrations/yolov5',
+        'guides/integrations/mmdetection',
+        'guides/integrations/scikit',
+        'guides/integrations/xgboost',
+        'guides/integrations/lightgbm',
+        {
+          type: 'category',
+          label: 'Fastai',
+          items: [
+            'guides/integrations/fastai/intro',
+            'guides/integrations/fastai/v1',
+          ]
+        },
+      ]
+    },
   ],
   howtoguides: [
     'howtoguides/intro',
