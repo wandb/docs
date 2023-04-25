4391905eccf6be90fbc4846fe7007bd806b9c5d7
jwlee64 <josiah.lee@wandb.com>
2023-04-25 16:48:33 -0700

    restyled right sidebar

---
+++ src/theme/TOC/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import clsx from 'clsx';
+import TOCItems from '@theme/TOCItems';
+import styles from './styles.module.css';
+// Using a custom className
+// This prevents TOCInline/TOCCollapsible getting highlighted by mistake
+const LINK_CLASS_NAME = 'table-of-contents__link toc-highlight';
+const LINK_ACTIVE_CLASS_NAME = 'table-of-contents__link--active';
+export default function TOC({className, ...props}) {
+  return (
+    <div className={clsx(styles.tableOfContents, 'thin-scrollbar', className)}>
+      {/* the only thing added in this swizzle was this header */}
+      <span className='TOC-header'>On this page</span>
+      <TOCItems
+        {...props}
+        linkClassName={LINK_CLASS_NAME}
+        linkActiveClassName={LINK_ACTIVE_CLASS_NAME}
+      />
+    </div>
+);
+}
