3ca19be2e79592ea60eb394ac0a03b21b77adb99
Thomas Capelle <tcapelle@pm.me>
2023-07-07 00:14:28 +0200

    working action

--- scripts/translate.py
+++ scripts/translate.py
@@ -1,25 +1,28 @@
 from pathlib import Path
-
 from gpt_translate.translate import _translate_file
 
 
 DOCS_FOLDER = Path("docs")
-CHANGED_FILES = Path("changed_md_files.txt")
-OUT_DIR = Path("i18n/docusaurus-plugin-content-docs/current")
+CHANGED_FILES = Path("changed_files.txt")
+OUT_DIR = Path("docs_ja")
 LANGUAGE = "ja"
 MODEL = "gpt-4"
+VERBOSE = True
 
 def translate(file=CHANGED_FILES, out_dir=OUT_DIR):
     with open(file, "r") as f:
         files = f.readlines()
     files = [Path(file.strip()) for file in files]
+    files = [f for f in files if f.suffix == ".md"]
+    print("Modified files:", files)
     for file in files:
         out_file = OUT_DIR / file.relative_to(DOCS_FOLDER)
         _translate_file(input_file=file, 
                         out_file=out_file,
                         language=LANGUAGE,
                         model=MODEL,
-                        verbose=True)
-        
-translate()
+                        verbose=VERBOSE)
+
+if __name__ == "__main__":        
+    translate()
 
