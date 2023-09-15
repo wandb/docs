238053a036afd5655191e0df2f6bc7736dc20c98
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2023-09-15 10:16:03 -0700

    Added Farama into sidebar.js

--- sidebars.js
+++ sidebars.js
@@ -350,6 +350,7 @@ const sidebars = {
         'guides/integrations/other/deepchecks',
         'guides/integrations/other/deepchem',
         'guides/integrations/other/docker',
+        'guides/integrations/other/farama-gymnasium',
         {
           type: 'category',
           label: 'Fastai',
