2feff234fbd38927467eac306336e861398a7b5f
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2024-02-12 21:49:35 -0800

    Updated scripts

---
+++ scripts/build-prod-docs.sh
@@ -0,0 +1,10 @@
+#!/bin/usr
+subdir="ko"
+
+# Build the docs and store them in the correct build subdir
+yarn docusaurus build --out-dir build/$subdir --config docusaurus.config.js
+
+echo "Formatting $language docs for GAE..."
+
+cp -R ./build/$subdir/img ./build/
+cp ./build/$subdir/index.html ./build/
