7055a9ccd965eb7b2e16b8a41263d59269f9eaed
johndmulhausen <john.mulhausen@wandb.com>
2024-09-20 19:25:23 -0400

    noclone

--- scripts/production.sh
+++ scripts/production.sh
@@ -2,12 +2,8 @@
 if [ "$CF_PAGES_BRANCH" == "staging" ]; then
     yarn build:prod
 else
-    rm -rf ../build
-    git config --global http.version HTTP/1.1
-    git clone --depth 1 https://github.com/wandb/docodile ./docodile
-    cd docodile
-    git remote set-branches origin '*'
-    git fetch -v --depth=1
+    cd ../
+    rm -rf build
     git checkout japanese_docs
     bash ./scripts/build-prod-docs.sh
     mv build ../build-ja
@@ -18,7 +14,6 @@ else
     yarn build:prod
     mv build ../build-en
     cd ..
-    rm -rf docodile
     mv build-ja/ja build-en/ja
     mv build-ko/ko build-en/ko
     mv build-en ../build
