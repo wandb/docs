26ab388afdf76911b3e1bbea8b445387c4f45a2e
johndmulhausen <john.mulhausen@wandb.com>
2024-09-20 19:40:59 -0400

    Final

--- scripts/production.sh
+++ scripts/production.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-if [ "$CF_PAGES_BRANCH" == "staging" ]; then
-    yarn build:prod
-else
-    cd ../
-    rm -rf build
-    yarn build:prod
-    mv build ..
-    git checkout japanese_docs
-    bash ./scripts/build-prod-docs.sh
-    mv build ../build/ja
-    git checkout korean_docs
-    bash ./scripts/build-prod-docs.sh
-    mv build/ko ../build
-fi
+cd ../
+rm -rf build
+yarn build:prod
+mv build ..
+git checkout japanese_docs
+bash ./scripts/build-prod-docs.sh
+mv build ../build/ja
+git checkout korean_docs
+bash ./scripts/build-prod-docs.sh
+mv build/ko ../build
