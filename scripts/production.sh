d8ce16af6c2e008c61f45abb4a448f0c4cb488d7
johndmulhausen <john.mulhausen@wandb.com>
2024-09-20 21:09:53 -0400

    within scripts dir

--- scripts/production.sh
+++ scripts/production.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
+rm -rf scripts/docodile
+git clone https://github.com/wandb/docodile.git scripts/docodile
+cd scripts/docodile
 git fetch
-rm -rf build
 yarn build:prod
-mv build ..
+mv build ../..
 git checkout japanese_docs
 bash ./scripts/build-prod-docs.sh
-mv build/ja ../build
+mv build/ja ../../build
 git checkout korean_docs
 bash ./scripts/build-prod-docs.sh
-mv build/ko ../build
-git checkout main
-rm -rf build
-mv ../build .
+mv build/ko ../../build
