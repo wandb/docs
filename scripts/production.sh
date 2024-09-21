3340be5c993c81df36bfb8bbdb501d69af969d46
johndmulhausen <john.mulhausen@wandb.com>
2024-09-21 12:59:17 -0400

    v2

--- scripts/production.sh
+++ scripts/production.sh
@@ -1,13 +1,25 @@
-#!/bin/bash
-rm -rf scripts/docodile
-git clone https://github.com/wandb/docodile.git scripts/docodile
-cd scripts/docodile
-git fetch
+# Run this from root by calling sh ./scripts/production.sh
+
+# Build the English docs from this branch
 yarn build:prod
-mv build ../..
+
+# Use a secondary git clone to build JA and KO 
+rm -rf scripts/docodile
+git clone --depth 1 https://github.com/wandb/docodile.git scripts/docodile
+cd scripts/docodile
+git remote set-branches origin '*'
+git fetch -v --depth=1
+
+# The JA and KO builds use special scripts for the baseURL mod
+# JA
 git checkout japanese_docs
-bash ./scripts/build-prod-docs.sh
+sh scripts/build-prod-docs.sh
 mv build/ja ../../build
+# KO
 git checkout korean_docs
-bash ./scripts/build-prod-docs.sh
+sh scripts/build-prod-docs.sh
 mv build/ko ../../build
+
+# Cleanup
+cd ../..
+rm -rf scripts/docodile
