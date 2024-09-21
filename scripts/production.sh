a0317dd5c2a6694d2093f66b97cc00b34a854fc2
johndmulhausen <john.mulhausen@wandb.com>
2024-09-20 20:48:39 -0400

    attempt

--- scripts/production.sh
+++ scripts/production.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git fetch
-cd ../
 rm -rf build
 yarn build:prod
 mv build ..
@@ -10,3 +9,4 @@ mv build ../build/ja
 git checkout korean_docs
 bash ./scripts/build-prod-docs.sh
 mv build/ko ../build
+mv ../build .
