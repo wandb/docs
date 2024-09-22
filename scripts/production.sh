b8ccbf6dc48849f6b8c2d64f287e51f4ddd0924e
John Mulhausen <john.mulhausen@wandb.com>
2024-09-22 01:03:29 -0400

    I18n fix (#830)
    Fixes JA and KO translations by building them into the production output
    via `/scripts/production.sh`

---
+++ scripts/production.sh
@@ -0,0 +1,27 @@
+# Run this from root by calling sh ./scripts/production.sh
+
+# Build the English docs from this branch
+yarn build:prod
+
+# Use a secondary git clone to build JA and KO 
+rm -rf scripts/docodile
+git clone --depth 1 git@github.com:wandb/docodile.git scripts/docodile
+cd scripts/docodile
+git remote set-branches origin '*'
+git fetch -v --depth=1
+
+# The JA and KO builds use special scripts for the baseURL mod
+# JA
+git checkout japanese_docs
+yarn install
+sh scripts/build-prod-docs.sh
+mv build/ja ../../build
+# KO
+git checkout korean_docs
+yarn install
+sh scripts/build-prod-docs.sh
+mv build/ko ../../build
+
+# Cleanup
+cd ../..
+rm -rf scripts/docodile
