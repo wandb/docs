874f2d13feb104c86a91e6fc9811296329142eb7
johndmulhausen <john.mulhausen@wandb.com>
2024-12-20 14:24:39 -0500

    Production cleanup

--- scripts/production.sh
+++ scripts/production.sh
@@ -6,21 +6,23 @@ rm -rf public
 hugo
 
 # Use a secondary git clone to build JA and KO 
-rm -rf scripts/docodile
-git clone --depth 1 git@github.com:wandb/docodile.git scripts/docodile
-cd scripts/docodile
+rm -rf scripts/docs
+git clone --depth 1 git@github.com:wandb/docs.git scripts/docs
+cd scripts/docs
 git remote set-branches origin '*'
 git fetch -v --depth=1
 
 # The JA and KO builds use special scripts for the baseURL mod
 # JA (requires node 18.0.0)
 git checkout japanese_docs
+asdf install nodejs 18.0.0
 yarn install
 sh scripts/build-prod-docs.sh
 mv build/ja ../../public
 git stash
 # KO
 git checkout korean_docs
+asdf install nodejs 18.0.0
 yarn install
 sh scripts/build-prod-docs.sh
 mv build/ko ../../public
