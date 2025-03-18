ff83f15a17799ed83e23fccb5c8493ffff8693ba
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2025-03-17 17:02:20 -0700

    Fixed requirements.txt file paht

--- code_examples/check_dependencies.sh
+++ code_examples/check_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+REQUIREMENTS_PATH="code_examples/requirements.txt"
+pip install -r "$REQUIREMENTS_PATH" && echo "All packages installed successfully!" || echo "Installation failed."
 
-pip install -r requirements.txt && echo "All packages installed successfully!" || echo "Installation failed."
-
-python -c "import pkgutil; import sys; packages = [pkg.strip() for pkg in open('requirements.txt')]; missing = [p for p in packages if not pkgutil.find_loader(p)]; sys.exit(1) if missing else print('✅ All packages are installed.')"
+python -c "import pkgutil; import sys; packages = [pkg.strip() for pkg in open('$REQUIREMENTS_PATH')]; missing = [p for p in packages if not pkgutil.find_loader(p)]; sys.exit(1) if missing else print('✅ All packages are installed.')"
