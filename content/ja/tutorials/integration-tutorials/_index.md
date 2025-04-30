6ddb7832b4ab3b8b75253959fc0773505f67da46
Matt Linville <matt@linville.me>
2025-04-30 13:05:11 -0700

    Revert "Revert "Refresh translations and migrate them to Hugo" (#1289)"
    This reverts commit 7b1068db566982b455fa9433be6945e92848bcf6.

---
+++ content/ja/tutorials/integration-tutorials/_index.md
@@ -0,0 +1,12 @@
+---
+title: インテグレーションチュートリアル
+cascade:
+- url: tutorials/:filename
+menu:
+  tutorials:
+    identifier: ja-tutorials-integration-tutorials-_index
+    parent: tutorials
+type: docs
+url: tutorials/integration-tutorials
+---
+
--- scripts/production.sh
+++ scripts/production.sh
@@ -29,33 +29,3 @@ fi
 # Move English sitemap into root
 rm public/sitemap.xml
 mv public/en/sitemap.xml public/
-
-# Clear Hugo translated content, which we don't yet use
-rm -rf public/ja
-rm -rf public/ko
-
-# Use a secondary git clone to build JA and KO 
-rm -rf scripts/docs
-git clone --depth 1 git@github.com:wandb/docs.git scripts/docs
-cd scripts/docs
-git remote set-branches origin '*'
-git fetch -v --depth=1
-
-# The JA and KO builds use special scripts for the baseURL mod
-# JA (requires node 18.0.0)
-git checkout japanese_docs
-npm -g install yarn
-yarn install
-sh scripts/build-prod-docs.sh
-mv build/ja ../../public/ja
-git stash
-# KO
-git checkout korean_docs
-npm -g install yarn
-yarn install
-sh scripts/build-prod-docs.sh
-mv build/ko ../../public/ko
-git stash
-# Cleanup
-cd ../..
-rm -rf scripts/docs
