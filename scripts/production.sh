2ceee7bdbf8417473af659b216743dba2f0b2119
Matt Linville <matt.linville@wandb.com>
2025-05-27 15:18:40 -0700

    Dynamically set the Hugo version for Cloudflare in production.sh

--- scripts/production.sh
+++ scripts/production.sh
@@ -1,5 +1,8 @@
 # Run this from root by calling sh ./scripts/production.sh
 
+# Get the Hugo version for Cloudflare to use
+export HUGO_VERSION=$(cat ".hugo-version")
+
 # Build the English docs from this branch
 
 # Update Hugo if necessary
@@ -10,6 +13,7 @@ rm -rf public
 # Detect whether we are in Cloudflare
 if [ -n "$CF_PAGES" ] && [ -n "$CF_PAGES_URL" ]; then
     echo "Building in Cloudflare with these variables:"
+    echo "  HUGO_VERSION: $HUGO_VERSION"
     echo "  CF_PAGES: $CF_PAGES"
     echo "  CF_PAGES_URL: $CF_PAGES_URL"
     echo "  CF_PAGES_COMMIT_SHA: $CF_PAGES_COMMIT_SHA"
@@ -22,7 +26,7 @@ if [ -n "$CF_PAGES" ] && [ -n "$CF_PAGES_URL" ]; then
         hugo -b $CF_PAGES_URL
     fi
 else
-    echo "Building locally"
+    echo "Building locally with Hugo" $(cat .hugo-version)
     hugo
 fi
 
