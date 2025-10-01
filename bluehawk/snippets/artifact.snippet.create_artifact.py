f485ef5384df57c0ed7cd9cc03c89635b78aa172
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2025-10-01 11:24:06 -0700

    adds artifact code blocks

---
+++ bluehawk/snippets/artifact.snippet.create_artifact.py
@@ -0,0 +1,4 @@
+run = wandb.init(project="artifacts-example", job_type="add-dataset")
+artifact = wandb.Artifact(name="example_artifact", type="dataset")
+artifact.add_file(local_path="./dataset.h5", name="training_dataset")
+artifact.save()
---
+++ bluehawk/snippets/artifact.snippet.download_artifact-1.py
@@ -0,0 +1,3 @@
+artifact = run.use_artifact(
+    "training_dataset:latest"
+)  # returns a run object using the "my_data" artifact
