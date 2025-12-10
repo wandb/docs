4af37a41936993239b229c73dfed03d9b2f09d10
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2025-12-09 16:46:51 -0800

    Add more Artifact code examples

---
+++ .ai/skills/artifacts_download.py
@@ -0,0 +1,16 @@
+"""
+Downloads files or entire artifacts from W&B.
+"""
+
+import wandb    
+
+with wandb.init(project="<project>") as run:
+    # Indicate the artifact to use. Format is "name:alias"
+    artifact = run.use_artifact("<artifact_name>:<alias>")
+
+    # Downloads file from the artifact at path name
+    # If artifact.add_reference() was used, returns the reference URL
+    entry = artifact.get_entry("<file_name>")
+
+    # Download the entire artifact
+    datadir = artifact.download()
